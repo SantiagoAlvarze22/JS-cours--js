a5f1b9ed4406ea8a6bb345b0f53f3c6db2f5dc46
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-21 19:09:12 -0500

    js

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('js is ');
+
+// console.log(7 + 77 + 10);
+
+let firstName = 'Santi';
+// console.log(firstName);
+
+//LECTURE: Values and Variables
+
+/*. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console */
+
+let country = 'Colombia';
+let continent = 'America';
+let population = 58000000;
+
+// console.log(country,
+//   continent,
+//   population)
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+console.log(typeof javascriptIsFun) //boolean
+// console.log(typeof population)
+// console.log(typeof continent)
+
+// javascriptIsFun = 'JsIsFun';
+console.log(typeof javascriptIsFun)
+
+//undefined
+
+let year;
+console.log(typeof year)
