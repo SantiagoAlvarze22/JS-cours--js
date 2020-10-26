1f7f5da8434978ea90b24d1774242377dd84477c
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-26 12:34:37 -0500

    challenge 4

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -397,21 +397,58 @@ Test data:
 
 //ternary operator 
 
-const age = 20;
+// const age = 20;
 
 // age >= 18 ? console.log('es mayor de edad') : console.log('no lo es');
 
-const drink = age >= 18 ? 'wine' : 'water';
-console.log(drink)
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink)
 
-let drink2;
-if (age >= 18) {
-  drink2 = 'wine';
-} else {
-  drink2 = 'water'
-}
+// let drink2;
+// if (age >= 18) {
+//   drink2 = 'wine';
+// } else {
+//   drink2 = 'water'
+// }
 
-console.log(drink2)
+// console.log(drink2)
 
 //ternary operator is an expression because it returns a value
-console.log(`I want to drink ${age >= 18 ? 'wine' : 'water'}`)
+// console.log(`I want to drink ${age >= 18 ? 'wine' : 'water'}`)
+
+/*
+Coding Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 �
+*/
+let bills = 275;
+let value = bills >= 50 && bills <= 300;
+
+let tip = value ? bills * 0.15 : bills * 0.2;
+
+console.log(`The bill was $${bills}, the tip was $${tip} and the total value $${bills + tip}`)
+
+bills = 40;
+value = bills >= 50 && bills <= 300
+tip = value ? bills * 0.15 : bills * 0.2;
+
+console.log(`The bill was $${bills}, the tip was $${tip} and the total value $${bills + tip}`)
+
+bills = 430;
+value = bills >= 50 && bills <= 300
+tip = value ? bills * 0.15 : bills * 0.2;
+console.log(`The bill was $${bills}, the tip was $${tip} and the total value $${bills + tip}`)
