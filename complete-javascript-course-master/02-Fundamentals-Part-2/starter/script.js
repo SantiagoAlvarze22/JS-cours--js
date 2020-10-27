eca5ad494e0db9b11dac2cbcd2c9686c42eb7021
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-27 09:57:51 -0500

    arrow functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -31,19 +31,34 @@
 
 //function declaration || function statement
 
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-  // const age = 2037 - birthYear;
-  // return age;
-}
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+//   // const age = 2037 - birthYear;
+//   // return age;
+// }
 
-const age1 = calcAge1(1991);
-console.log(age1)
+// const age1 = calcAge1(1991);
+// console.log(age1)
 
 //Function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+
+
+// const age2 = calcAge2(1991)
+// console.log(age1, age2)
+
+//arraw function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+// console.log(age3)
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`
 }
 
-const age2 = calcAge2(1991)
-console.log(age1, age2)
+console.log(yearsUntilRetirement(1991, 'Santiago Alvarez'))
