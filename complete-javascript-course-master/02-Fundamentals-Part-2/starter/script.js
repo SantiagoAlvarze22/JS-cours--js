706dcf5e328e4500b65e7d27589d86d120426066
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-27 11:49:05 -0500

    challenge function 1

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -63,16 +63,78 @@
 
 // console.log(yearsUntilRetirement(1991, 'Santiago Alvarez'))
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3))
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   // return retirement;
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired`)
+//     return -1;
+//   }
+
+// }
+
+// yearsUntilRetirement(1991, 'Santi')
+
+/*Coding Challenge #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27 */
+
+const calcAverage = (sc1, sc2, sc3) => (sc1 + sc2 + sc3) / 3
+
+const averageD1Dolphins = calcAverage(44, 23, 71);
+const averageD1Koalas = calcAverage(65, 54, 49);
+
+const averageD2Dolphins = calcAverage(85, 54, 41);
+const averageD2Koalas = calcAverage(23, 34, 27);
+
+function checkWinner(avgDolhins, avgKoalas) {
+  console.log(avgDolhins, avgKoalas)
+  if (avgDolhins > (2 * avgKoalas)) {
+    console.log(`Dolphins win (${avgDolhins} vs ${avgKoalas})`)
+  } else if (avgKoalas > (2 * avgDolhins)) {
+    console.log(`Kolas win (${avgKoalas} vs ${avgDolhins})`)
+  } else {
+    console.log('No one Win!!!')
+  }
 }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-
-console.log(fruitProcessor(2, 3))
+checkWinner(averageD1Dolphins, averageD1Koalas)
