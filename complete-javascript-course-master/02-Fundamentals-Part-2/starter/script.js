0985078065c4991d0a4a46675eb894e82c3e4400
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-27 15:24:33 -0500

    challenge array

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -176,36 +176,71 @@ Test data:
 // const ages = [age1, age2, age3]
 // console.log(ages)
 
-const friends = ['Michael', 'Steven', 'Peter'];
-console.log(friends)
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends)
 
-//Add element
-//push method 
-const newLength = friends.push('Jay')
-console.log(friends)
-console.log(newLength)
+// //Add element
+// //push method 
+// const newLength = friends.push('Jay')
+// console.log(friends)
+// console.log(newLength)
 
-//unshift
-friends.unshift('John')
-console.log(friends)
+// //unshift
+// friends.unshift('John')
+// console.log(friends)
 
-//Remove elements
+// //Remove elements
 
-//pop
+// //pop
 
-const popped = friends.pop(); //return the remove value
-console.log(popped)
-console.log(friends)
+// const popped = friends.pop(); //return the remove value
+// console.log(popped)
+// console.log(friends)
 
-//shift
-friends.shift()
-console.log(friends)
+// //shift
+// friends.shift()
+// console.log(friends)
 
-console.log(friends.indexOf('Steven')) //index which the element is located
-console.log(friends.indexOf('blue')) //-1 bc does not exist
+// console.log(friends.indexOf('Steven')) //index which the element is located
+// console.log(friends.indexOf('blue')) //-1 bc does not exist
 
-console.log(friends.includes('Steven')); //return a Boolean
+// console.log(friends.includes('Steven')); //return a Boolean
 
-if (friends.includes('Steven')) {
-  console.log('call Steven')
+// if (friends.includes('Steven')) {
+//   console.log('call Steven')
+// }
+
+/*Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44 */
+
+
+const calcTip = function (billValue) {
+  if (billValue >= 50 && billValue <= 300) {
+    return billValue * 0.15;
+  } else {
+    return billValue * 0.20;
+  }
 }
+
+const tip = calcTip(100);
+console.log(tip);
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
