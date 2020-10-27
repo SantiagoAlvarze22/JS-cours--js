d7501a411e2a0673e14398e1547cdcfbe275dd2c
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-27 10:52:13 -0500

    functions calling other functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -49,16 +49,30 @@
 // const age2 = calcAge2(1991)
 // console.log(age1, age2)
 
-//arraw function
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1991);
-// console.log(age3)
+// //arraw function
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// // console.log(age3)
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  // return retirement;
-  return `${firstName} retires in ${retirement} years`
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Santiago Alvarez'))
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
 
-console.log(yearsUntilRetirement(1991, 'Santiago Alvarez'))
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3))
