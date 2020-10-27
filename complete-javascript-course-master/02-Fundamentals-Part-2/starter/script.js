54a2e2fdddbb76265112bda5067be676df5c38da
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-27 09:23:37 -0500

    functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,30 @@
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTess = true;
+// let hasDriversLicense = false;
+// const passTess = true;
 
-if (passTess) hasDriverLicense = true;
-if (hasDriversLicense) console.log('I can drive :D')
+// if (passTess) hasDriverLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D')
+
+function logger() {
+  console.log('My name is Santi');
+}
+
+//calling, running or invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+//if I want to capture the value that return the function
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice)
+
+//I can  call the value just setting the same function without getting apart the value
+console.log(fruitProcessor(5, 0))
+
+const appleOrangeJucie = fruitProcessor(2, 4)
+console.log(appleOrangeJucie)
