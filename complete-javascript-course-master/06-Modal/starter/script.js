8f6960b221991490b91e82d847fa6683bf3cf319
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-29 10:52:25 -0500

    open and close modal functionalities

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -5,4 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+//functions
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//addEventslisteners
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
