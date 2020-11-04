33c8f563cb22062841ced1d7810e97802960deb5
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-04 16:29:49 -0500

    enhace object literals

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,13 +1,31 @@
 'use strict';
 
+const weekdays = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sund'];
+const openingHours1 = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[5]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[6]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours1,
 
-  order: function (starterIndex, mainIndex) {
+  //new method to write a function
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
