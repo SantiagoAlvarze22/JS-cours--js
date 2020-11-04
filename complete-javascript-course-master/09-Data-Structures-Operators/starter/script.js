5935563095826e7af09eea533d6c4b8bb82259f8
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-04 12:54:04 -0500

    challenge #1 ata structure

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -61,11 +61,11 @@ restaurant.numGuess = 20;
 
 restaurant.numGuess = 10;
 const guests2 = restaurant.numGuess || 10;
-console.log(guests2);
+// console.log(guests2);
 
 //Nullish: null and undefined(NOT 0 or '')
 const guestsCorrect = restaurant.numGuess ?? 10;
-console.log(guestsCorrect);
+// console.log(guestsCorrect);
 
 // console.log(0 && 'jonas'); //when the first value is falsy will be returning that value inmmediately (it doesn't matter if it is not the first one value, it must be the first value in de line)
 
@@ -235,3 +235,125 @@ const [i, , [j, k]] = nested;
 //default values
 const [p, q, r] = [8, 9];
 // console.log(p, q, r);
+
+/*Coding Challenge #1
+We're building a football betting app (soccer for my American friends �)!
+Suppose we get data from a web service about a certain game ('game' variable on
+next page). In this challenge we're gonna work with that data.
+Your tasks:
+1. Create one player array for each team (variables 'players1' and
+'players2')
+2. The first player in any player array is the goalkeeper and the others are field
+players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22
+players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+new array ('players1Final') containing all the original team1 players plus
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
+operator.
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+Then, call the function again with players from game.scored */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+const [gk1, ...fieldPlayers1] = game.players[0];
+const [gk2, ...fieldPlayers2] = game.players[1];
+
+// const { players1, players2 } = game.players;
+
+const players1 = [gk1, ...fieldPlayers1];
+const players1Final = [...players1, 'Thiago', 'Cautinho', 'Perisio'];
+// console.log(players1Final);
+
+const players2 = [gk2, ...fieldPlayers2];
+
+// console.log(...players1, ...players2);
+
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// const team1 = game.odds['team1'];
+// const draw = game.odds['x'];
+// const team2 = game.odds['team2'];
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+//team most likely to win
+// console.log(team1, team2);
+team1 < team2 && console.log('The team 1 is most likely to win');
+team1 > team2 && console.log('The team2 is most likely to win');
+
+// console.log(team1, draw, team2);
+
+const printGoals = (...nameOfPlayers) => {
+  console.log(`${nameOfPlayers.length} goals were score`);
+  let sum = 0;
+  for (let i = 0; i < nameOfPlayers.length; i++) {
+    if (
+      nameOfPlayers[i] === 'Lewandowski' ||
+      nameOfPlayers[i] === 'Gnarby' ||
+      nameOfPlayers[i] === 'Lewandowski' ||
+      nameOfPlayers[i] === 'Hummels'
+    ) {
+      console.log(`${nameOfPlayers[i]}: 1`);
+      sum += 1;
+    } else {
+      console.log(`${nameOfPlayers[i]}: 0`);
+    }
+  }
+  console.log(
+    `according to the score array, and comparing with the name of the players this is the real amount of goals ${sum}`
+  );
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
