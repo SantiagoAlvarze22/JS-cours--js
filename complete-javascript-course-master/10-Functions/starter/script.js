bf51aac5b9cd928775f12538fd086090858eef21
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-09 13:19:02 -0500

    default parameters

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(bookings);
+  bookings.push(booking);
+};
+
+createBooking(1292);
+createBooking(122, undefined, 12300);
