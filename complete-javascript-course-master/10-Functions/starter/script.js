aba2aab64646175324485598e1400608e0d51139
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-09 16:26:52 -0500

    passing arguments as a values

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -16,5 +16,38 @@ const createBooking = function (
   bookings.push(booking);
 };
 
-createBooking(1292);
-createBooking(122, undefined, 12300);
+// createBooking(1292);
+// createBooking(122, undefined, 12300);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 23425435232345,
+};
+
+function checkIn(flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 23425435232345) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+}
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+//is the same as doing
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = person => {
+  person.passport = Math.trunc(Math.random() * 1000000000000);
+};
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+// console.log(flight, jonas);
