625e43505a82b88a88a68f8480de18a1c3cc02bd
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-17 16:11:02 -0500

    the remainder operator

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -283,36 +283,57 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isInteger(23.0));
 // console.log(Number.isInteger(23 / 0)); //infinity
 
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
 
-console.log(Math.max(5, 18, 11, 2));
-console.log(Math.max(5, 18, '25', 11, 2));
+// console.log(Math.max(5, 18, 11, 2));
+// console.log(Math.max(5, 18, '25', 11, 2));
 
-console.log(Math.min(5, 18, 11, 2));
+// console.log(Math.min(5, 18, 11, 2));
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-console.log(Math.trunc(Math.random() * 6) + 1);
+// console.log(Math.trunc(Math.random() * 6) + 1);
 
-const randomInt = (min, max) =>
-  Math.trunc(Math.random() * (max - min) + 1) + min;
-console.log(randomInt(10, 20));
+// const randomInt = (min, max) =>
+//   Math.trunc(Math.random() * (max - min) + 1) + min;
+// console.log(randomInt(10, 20));
 
-//rounding integers
-console.log(Math.ceil(23.3)); //va hacia el entero mayor cercano
-console.log(Math.floor(23.3)); //va hacia el entero menor cercano
+// //rounding integers
+// console.log(Math.ceil(23.3)); //va hacia el entero mayor cercano
+// console.log(Math.floor(23.3)); //va hacia el entero menor cercano
 
-console.log(Math.round(23.9));
+// console.log(Math.round(23.9));
 
-//floor and trunc do the same when they're dealing with postive numbers
-console.log(Math.floor(23.3));
-console.log(Math.trunc(23.3));
+// //floor and trunc do the same when they're dealing with postive numbers
+// console.log(Math.floor(23.3));
+// console.log(Math.trunc(23.3));
 
-console.log(Math.floor(-23.3));
-console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
+// console.log(Math.trunc(-23.3));
 
-//rounding decimals
-console.log(+(2.3).toFixed(0)); //close to the next integer <.5 >.5
-console.log(+(2.2453).toFixed(3));
+// //rounding decimals
+// console.log(+(2.3).toFixed(0)); //close to the next integer <.5 >.5
+// console.log(+(2.2453).toFixed(3));
+
+console.log(5 % 2); //  2*2 +1
+console.log(8 % 3); // 8 = 2*3 +2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(2));
+console.log(isEven(3));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'orangered';
+    }
+    if (i % 3 === 0) {
+      row.style.backgroundColor = 'blue';
+    }
+  });
+});
