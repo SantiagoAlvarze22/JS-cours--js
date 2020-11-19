d4ec84dd2c719e40185b41bd327f3c97de5d36ca
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-19 17:29:48 -0500

    implementing smooth scrolling

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -29,92 +29,123 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//smooth scroll
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('current scrroll (x/y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //scrolling the s1coords is relative to the viewport and not to the document, what I do it just addiotion the current height the the top coord, and is now accorrding to the html document, absolute position relative to the document
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  //nowadays view
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 //slecting elemetns
-console.log(document.documentElement);
-console.log(document.body);
-console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body);
+// console.log(document.head);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
 //creating and inserting elements
 // .insertAdjacentHTML
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent =
-  'We use cookies for improving functionality and analytics';
-message.innerHTML = `We use cookies for improving functionality and analytics <button class="btn btn--close-cookie">Got it" </button>`;
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improving functionality and analytics';
+// message.innerHTML = `We use cookies for improving functionality and analytics <button class="btn btn--close-cookie">Got it" </button>`;
 
-header.prepend(message);
-// header.append(message);
+// header.prepend(message);
+// // header.append(message);
 
-// header.append(message.cloneNode(true));
+// // header.append(message.cloneNode(true));
 
-//inseert elementes as a sibling, after or
-// header.before(message);
-header.after(message);
+// //inseert elementes as a sibling, after or
+// // header.before(message);
+// header.after(message);
 
-//delete elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// //delete elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
 
-//styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// //styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(message.style.backgroundColor);
+// console.log(message.style.backgroundColor);
 
-//consult the properties that has the message element
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// //consult the properties that has the message element
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
 
-//cambiar propiedades que tengo
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// //cambiar propiedades que tengo
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-//Attributes, standard properties
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// //Attributes, standard properties
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
-//setting attributes
-logo.alt = 'beatiful minimalist logo';
+// //setting attributes
+// logo.alt = 'beatiful minimalist logo';
 
-//non-standard it doesn't work
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
+// //non-standard it doesn't work
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
 
-console.log(logo.src);
+// console.log(logo.src);
 
-//relative path
-console.log(logo.getAttribute('src'));
+// //relative path
+// console.log(logo.getAttribute('src'));
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
-//Data attributes, converting the data set propeties in camelcase
-console.log(logo.dataset.versionNumber);
+// //Data attributes, converting the data set propeties in camelcase
+// console.log(logo.dataset.versionNumber);
 
-//classes
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
+// //classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
 
-//don't use this bc it would overwrite all the logo classes
-logo.className = 'jonas';
+// //don't use this bc it would overwrite all the logo classes
+// logo.className = 'jonas';
