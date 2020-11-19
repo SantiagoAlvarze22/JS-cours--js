ea3812c9755e70990d73ca894deb8082ca54c929
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-19 16:36:25 -0500

    styles, attributes and classes

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -68,3 +68,53 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+//styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor);
+
+//consult the properties that has the message element
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+//cambiar propiedades que tengo
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//Attributes, standard properties
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+//setting attributes
+logo.alt = 'beatiful minimalist logo';
+
+//non-standard it doesn't work
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+console.log(logo.src);
+
+//relative path
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+//Data attributes, converting the data set propeties in camelcase
+console.log(logo.dataset.versionNumber);
+
+//classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+//don't use this bc it would overwrite all the logo classes
+logo.className = 'jonas';
