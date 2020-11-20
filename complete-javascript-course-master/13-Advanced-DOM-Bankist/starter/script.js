f7eaf83a5eac2ebee438958d8f4fa353564eb37c
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-20 14:36:56 -0500

    DOM traversing

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -216,3 +216,34 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 //   this.style.backgroundColor = randomColor(0, 255);
 //   console.log('Nav', e.target, e.currentTarget);
 // });
+
+const h1 = document.querySelector('h1');
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); //elements that are inside of the h1
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'blue';
+
+//going upwars --> selecting parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+//closest parent element
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//going sideways: sibling
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    el.style.transfom = 'scale(0.5)';
+  }
+});
