a925fafdd0d34cbe44ff6eaae6b1f04d28c28d63
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-26 18:40:33 -0500

    setter and getters

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -111,8 +111,8 @@
 
 //class declarations
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -124,16 +124,52 @@ class PersonCl {
   greet() {
     console.log(`Hey  ${this.firstName}`);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  //set a property that alreadt exist
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1995);
+const jessica = new PersonCl('Jessica Davis', 1995);
 console.log(jessica);
 jessica.calcAge();
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
 jessica.greet();
+console.log(jessica.age);
 
 //1. classes are NOT hoisted
 // 2. classes are first-class citizen
 //3. classes are executed in strict mode
+
+const walter = new PersonCl('walter bried', 1965);
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50; //set is just a property, an I invoke it as if I were setting a value
+console.log(account.movements);
