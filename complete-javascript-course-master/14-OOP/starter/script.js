841abf58a5f359f36c02a1530da8a55583d1cc9b
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-26 09:30:16 -0500

    constructor functions and new operator

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //never create a method inside of a constructor functions is A BAD PRACTICE, BC IT WOULD BE CREATED AS  MANY INSTANCES I CREATED, FOR THIS PROTYPE INHERITANCE
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+
+const matilda = new Person('Matilda', 2017);
+console.log(jonas, matilda);
+
+console.log(jonas instanceof Person);
+
+// 1. New {} is created
+// 2. functions is called, this = {}
+// 3. {} linked to prototype
+// 4. function automaticalle return {}
