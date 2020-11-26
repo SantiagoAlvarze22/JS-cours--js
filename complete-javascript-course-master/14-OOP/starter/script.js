3d0fd6a0cb552a29428433916e874d0d9427ca10
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-26 17:27:52 -0500

    challange 1 OOP

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -67,3 +67,41 @@ Array.prototype.unique = function () {
 console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
+
+// Object Oriented Programming (OOP)
+// Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+// 'speed' property. The 'speed' property is the current speed of the car in
+// km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10,
+// and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+// the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+// 'brake' multiple times on each of them
+// Test data:
+// § Data car 1: 'BMW' going at 120 km/h
+// § Data car 2: 'Mercedes' going at 95 km/h
+// GOOD LUCK �
+
+const Car = function (speed, make) {
+  this.speed = speed;
+  this.make = make;
+};
+
+Car.prototype.accelerate = function () {
+  console.log((this.speed += 10));
+};
+
+Car.prototype.brake = function () {
+  console.log((this.speed -= 5));
+};
+
+const bmw = new Car(120, 'BMW');
+bmw.accelerate();
+bmw.brake();
+
+const mer = new Car(95, 'Mercedes');
+mer.accelerate();
+mer.brake();
