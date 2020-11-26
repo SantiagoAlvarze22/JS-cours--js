7bd62bd86cfca76fff214bc5061948a92d079a80
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-26 17:10:48 -0500

    prototypal inheritance on built-in objects

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -35,9 +35,35 @@ jonas.calAge();
 matilda.calAge();
 console.log(jonas.__proto__ === Person.prototype);
 
+//Person.prototype, is not de PERSON prototype, are all the objects that were created by the Person function
+
 console.log(Person.prototype.isPrototypeOf(jonas));
 
 Person.prototype.species = 'Homo sapiens';
 console.log(jonas.species, matilda.speciesbg);
 
 console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+console.log(Person.prototype);
+console.log(jonas.__proto__.__proto__);
+console.log(Object.prototype);
+
+console.log(jonas.__proto__.__proto__.__proto__);
+console.log(Object.prototype.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [6, 7, 6, 9, 7, 8, 9]; // new Array === [] //
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
