d41ee2dea6064b95b7e6cabeded0ecd557ab58db
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-11-26 15:11:05 -0500

    prototypes

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -15,9 +15,29 @@ const jonas = new Person('Jonas', 1991);
 const matilda = new Person('Matilda', 2017);
 console.log(jonas, matilda);
 
-console.log(jonas instanceof Person);
+// console.log(jonas instanceof Person);
+
+//prototypes
+
+console.log(Person.prototype);
+
+//settting new method in my Person constructor function, so every instance of that constructor function will inheritance the method
+Person.prototype.calAge = function () {
+  console.log(2037 - this.birthYear);
+};
 
 // 1. New {} is created
 // 2. functions is called, this = {}
 // 3. {} linked to prototype
 // 4. function automaticalle return {}
+
+jonas.calAge();
+matilda.calAge();
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+Person.prototype.species = 'Homo sapiens';
+console.log(jonas.species, matilda.speciesbg);
+
+console.log(jonas.hasOwnProperty('species'));
