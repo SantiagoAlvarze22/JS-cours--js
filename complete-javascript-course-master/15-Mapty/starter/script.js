9d2eb6927207e511c599753a8122702a83fb3803
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-12-01 09:27:48 -0500

    using the Geolocation API

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//implementing the geolocatin, getCurrentPosition has two callback, the first one is the success if the map get the coordinates of the current postion and the other one is the one that is called if happened an error
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
