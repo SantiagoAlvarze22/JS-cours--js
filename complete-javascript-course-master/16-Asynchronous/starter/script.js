cf09f151e424a290b5fc034cbfe27e5ac1643b57
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-12-04 14:16:32 -0500

    callback hell

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -5,21 +5,44 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
 
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`); //to get data
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`); //to get data
 
-  request.send(); //sed the request to the url
+//   request.send(); //sed the request to the url
 
-  request.addEventListener('load', function () {
-    console.log(this.responseText);
+//   request.addEventListener('load', function () {
+//     console.log(this.responseText);
 
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-  <article class="country">
+//     const html = `
+//   <article class="country">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('portugal');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -33,11 +56,56 @@ const getCountryData = function (country) {
   </article>
   `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndeNeighbour = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`); //to get data
+
+  request.send(); //sed the request to the url
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //Render country
+    renderCountry(data);
+
+    //get neighbour country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+    //AJAX call country 1
+    const request2 = new XMLHttpRequest();
+
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`); //to get data
+
+    request2.send(); //send the request to the url
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('colombia');
-getCountryData('usa');
+getCountryAndeNeighbour('portugal');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
