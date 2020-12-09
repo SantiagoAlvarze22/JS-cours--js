97ef245c2659de02d26a0de9c26b243aa9a2eaff
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-12-09 18:17:50 -0500

    the module pattern

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -5,15 +5,42 @@
 
 // import * as ShoppingCart from './shoppingCart.js';
 
-console.log('Importing module');
+// console.log('Importing module');
 
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart.tq);
+// // ShoppingCart.addToCart('bread', 5);
+// // console.log(ShoppingCart.tq);
 
-// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-import add, { cart } from './shoppingCart.js';
+// // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// import add, { cart } from './shoppingCart.js';
 
-add('pizza', 3);
-add('apples', 20);
-add('oranges', 13);
-console.log(cart);
+// add('pizza', 3);
+// add('apples', 20);
+// add('oranges', 13);
+// console.log(cart);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shoppingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (prod, quantity) {
+    cart.push({ prod, quantity });
+    console.log(`${quantity} ${prod} added to the shopping cart`);
+  };
+
+  const orderStock = function (prod, quantity) {
+    cart.push({ prod, quantity });
+    console.log(`${quantity} ${prod} added to the shopping supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 2);
+console.log(ShoppingCart2);
